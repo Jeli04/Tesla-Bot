8bcddb6d01f50d6d4d8f8df71cae002a3a085cad
jetbot <jetbot@nano-4gb-jp45.localdomain>
2025-03-02 16:42:55 -0800

    experiment with movement

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import regex as re
 from client import * 
+import sys
+sys.path.append("/home/jetbot/jetbot")
+sys.path.append("/home/jetbot/jetbot/jetbot")
 from jetbot import Robot
+
 
 def main():
     # Initialize the robot
     robot = Robot()
 
     context = zmq.Context()
-    publisher_ip = "10.13.233.237"
+    publisher_ip = "10.10.159.66"
     topics = ["teslabot"]
     query = query_listener(context, publisher_ip, topics)
 
@@ -17,9 +21,11 @@ def main():
     # Fix the regex pattern - 'forawrd' seems to be misspelled and missing capture groups
     try:
         command = re.search(r"(forward)", query)
-        if command == "forward":
+        if command:
             print(f"Extracted command: {command.group(1)}")
-            robot.forward(0.5)
+            for i in range(100000):
+                #robot.forward(7.5)
+                robot.set_motors(0.6, 0.6)
         else:
             print("No command found in query")
     except AttributeError:
