4134028b043f8705f93be0327c88299417950227
Jerry <jeli04.2020@gmail.com>
2025-03-03 20:08:19 -0800

    movement update

--- agent.py
+++ agent.py
@@ -12,14 +12,16 @@ class TeslaBot():
         for command in commands:
             if command["action"] == "Move forward":
                 self.controls.forward(command["value"])
+                time.sleep(10)
             elif command["action"] == "Turn left":
                 self.controls.tesla_bot.turn_left(command["value"])
+                time.sleep(10)
             elif command["action"] == "Turn right":
                 self.controls.tesla_bot.turn_right(command["value"])
+                time.sleep(10)
             else:
                 print("invalid action")
                 exit() # TODO change later
-            time.sleep(5)
 
 
 
